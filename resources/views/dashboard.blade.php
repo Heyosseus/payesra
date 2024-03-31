25e168bdf77810d8a684aee0f866f72c3ce9fbc7
heyosseus <ratiruxadzee@gmail.com>
2024-03-31 12:54:16 +0400

    style: add blade views and improve styles

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+    <div class="flex items-center justify-between bg-gray-200 p-8">
+        <a href="/" class="text-4xl" >Paysera</a>
+        <div class="  ">
+            <a href="/profile" class="py-4 px-6 text-lg text-blue-900 rounded-3xl  hover:underline outline-0">Profile</a>
+            <a href="/logout" class="py-4 px-6 text-lg text-red-600  font-bold hover:underline    outline-0">Log out</a>
+        </div>
+    </div>
+</x-layout>
