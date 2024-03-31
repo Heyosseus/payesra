e4b1783b5d97d63616a8b69e0299295c288a6941
heyosseus <ratiruxadzee@gmail.com>
2024-03-31 14:53:27 +0400

    refactor: delete blade files and change web php

--- routes/web.php
+++ routes/web.php
@@ -11,19 +11,9 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
-|
+|/
 */
 
-Route::get('/', function () {
-    return view('home');
-});
-
-Route::controller(AuthController::class)->group(function () {
-    Route::get('/register', 'showRegisterForm')->name('register.show');
-    Route::post('/register', 'register')->name('register.store');
-    Route::get('/login', 'showLoginForm')->name('login.show');
-    Route::post('/login', 'login')->name('login.store');
-    Route::get('/logout', 'logout')->middleware('auth');
-});
-
-Route::view('/dashboard', 'dashboard')->name('dashboard')->middleware('auth');
+Route::get('/{any}', function () {
+    return view('dashboard');
+})->where('any', '.*');
